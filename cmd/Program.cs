7bfa4a7fc24cd56c2e7c1e35b8b152cdb13daeb2
Arad Aryan <javadjavad541@gmail.com>
2020-01-09 23:30:41 +0330

    Done

--- cmd/Program.cs
+++ cmd/Program.cs
@@ -1,94 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cmd
 {
     class Program
     {
+        #region Fields
+        /// <summary>
+        /// Output Directory of WinRAR
+        /// </summary>
         public static string outputPath;
+
+        /// <summary>
+        /// File Format .zip or .rar
+        /// </summary>
         public static bool type;
-        public static string[] files;
+
+        /// <summary>
+        /// Path of All Files 
+        /// </summary>
+        public static string files;
+        #endregion
+
+        #region Run Compress Command
+        /// <summary>
+        /// Run Compress Command For WinRAR.EXE
+        /// </summary>
+        static public void RunCommand()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "WinRAR.exe";
+            startInfo.Arguments = $"a {outputPath} {files}";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            Console.Write("File Created at: ");
+            Console.WriteLine(outputPath);
+            Console.Write("Format: ");
+            if (type)
+                Console.WriteLine("zip");
+            else
+                Console.WriteLine("rar");
+            Console.Write("Contains: ");
+            Console.WriteLine(files);
+        }
+        #endregion
+
+        #region Main
+        /// <summary>
+        /// Main Function
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            files = new string[args.Length - 4];
-
             if (args.Length == 0)
             {
-                Console.Write("Please Enter a Command");
+                Console.Write("Wrong Usage!");
             }
             else
             {
                 var command = args[0];
-                // Console.Write(command);
-                int counter = 0;
+                int counter = -1;
+
                 foreach (var item in args)
                 {
-                    if (item.ToLower() == "-out" )//&& args[counter + 1].StartsWith("\"") && args[counter + 1].EndsWith("\""))
-                    {
-                        outputPath = args[counter + 1];
-                    }
-                    //else
-                    //{
-                    //    Console.WriteLine("Wrong Value For output Address!");
-                    //    break;
-                    //}
-                    if (item.ToLower() == "-format" && (args[counter + 1].ToLower() == "zip" || args[counter + 1].ToLower() == "rar"))
-                    {
-                        if (args[counter + 1].ToLower() == "zip")
-                            type = true;
-
-                        if (args[counter + 1].ToLower() == "rar")
-                            type = false;
-                    }
-                    //else
-                    //{
-                    //    Console.WriteLine("Wrong Value For File Format, please choose [zip] or [rar]");
-                    //    break;
-                    //}
-                    if (item.ToLower() == "-files")
-                    {
-                        for (int i = 1; i <= args.Length - 4; i++)
-                        {
-                            if (!(counter + i >= args.Length))
-                            {
-                                // if (args[counter + i].StartsWith("\"") && args[counter + i].EndsWith("\""))
-                                // {
-                                files[i - 1] = args[counter + 1];
-                                // }
-                            }
-                            else
-                            {
-                                if (files.Length < 1)
-                                {
-                                    Console.WriteLine("Wrong Files Selected!");
-                                    break;
-                                }
-                            }
-                            
-                        }
-
-                    }
-                    //else
-                    //{
-                    //    Console.WriteLine("Wrong Value For File Format, please choose [zip] or [rar]");
-                    //    break;
-                    //}
                     counter++;
-                }
+                    switch (item.ToLower())
+                    {
+                        case "-out":
+                            outputPath = $"{ args[counter + 1]}";
+                            continue;
 
+                        case "-format":
+                            switch (args[counter + 1])
+                            {
+                                case "zip":
+                                    type = true;
+                                    break;
+
+                                case "rar":
+                                    type = false;
+                                    break;
+                            }
+                            continue;
+
+                        case "-files":
+                            int temp = counter + 1;
+                            for (int i = 0; i < args.Length - 4; i++)
+                            {
+                                if (!(temp >= args.Length))
+                                {
+                                    files += $"{args[temp]} ";
+                                    temp++;
+                                    continue;
+                                }
+                                break;
+                            }
+                            break;
+                    }
+                }
             }
-            Console.WriteLine(outputPath);
-            Console.WriteLine(type);
-            foreach (var item in files)
-            {
-                Console.Write(item + ", ");
-            }
+
+            if (type)
+                outputPath += ".zip";
+            else
+                outputPath += ".rar";
+
+            RunCommand();
 
             Console.ReadKey();
         }
-
+        #endregion
     }
 }
